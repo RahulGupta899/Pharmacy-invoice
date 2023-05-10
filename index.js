c0471a9768b542241b84442886b29f42ac7b3daf
RahulGupta899 <rgupta.jan@gmail.com>
2023-05-10 12:56:17 +0530

    recent updates

---
+++ index.js
@@ -0,0 +1,82 @@
+const button = document.querySelector('.btn');
+const csvButton = document.querySelector('.csvButton')
+const tableRows = document.querySelector('.table_rows')
+
+const buyer = document.querySelector('.buyer')
+const date = document.querySelector('.date')
+const invoiceNo = document.querySelector('.invoiceNo')
+
+
+
+let srNo = 0;
+
+
+// EVENT LISTENERS
+button.addEventListener('click', handleButtonClick)
+csvButton.addEventListener('click', exportCSV)
+
+
+// CSV RELATED TASK
+function exportCSV(){
+
+    // INVOICE DETAILS
+    const buyerValue = buyer.value;
+    const dateValue = date.value;
+    const invoiceValue = invoiceNo.value
+
+    const csvData = []
+
+    csvData.push(["Buyer's Name", "Date", "Invoice No"])
+    csvData.push([buyerValue, dateValue, invoiceValue])
+    csvData.push([])
+    csvData.push([])
+
+
+    // INVOICE RECORDS
+    const records = document.querySelectorAll("tr");
+     //FIELDS
+    let fields = Array.from(records[0].children)
+    fields = fields.map((column)=>column.textContent )
+    csvData.push(fields)
+     // ROWS
+    for(let i=1; i<records.length; i++){
+        let row = Array.from(records[i].children)
+        row = row.map((data,idx)=>{
+            if(idx === 0) return data.textContent;
+            else return data.children[0].value;
+        })
+        csvData.push(row)
+    }
+
+
+
+
+
+
+    
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.aoa_to_sheet(csvData);
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'PharmacyInvoice');
+    
+    XLSX.writeFile(workbook, 'PharmacyInvoice.xlsx');
+}
+
+
+// ADD ROW
+function handleButtonClick(){
+    srNo++;
+    const html = `
+    <tr>
+        <td>${srNo}</td>
+        <td><input class="input_large"/></td>
+        <td><input/></td>
+        <td><input/></td>
+        <td><input/></td>
+        <td><input/></td>
+        <td><input/></td>
+        <td><input/></td>
+        <td><input/></td>
+    </tr>
+    `
+    tableRows.innerHTML+= html;
+}
